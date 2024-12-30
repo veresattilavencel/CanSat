a535f29877906849f016e56918f34e91d9b0ee2f
Veres Attila Vencel <V.Attila.Vencel@gmail.com>
2024-12-30 17:00:39 +0000

    SerialPort decodes CRC

---
+++ SerialPort/ClassLibrary/CRC8.cs
@@ -0,0 +1,37 @@
+﻿namespace ClassLibrary
+{
+    public class CRC8
+    {
+        private const byte Polynomial = 0x07;
+        private static readonly byte[] CrcTable;
+        static CRC8()
+        {
+            CrcTable = new byte[256];
+            for (int i = 0; i < 256; i++)
+            {
+                byte current = (byte)i;
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((current & 0x80) != 0)
+                    {
+                        current = (byte)((current << 1) ^ Polynomial);
+                    }
+                    else
+                    {
+                        current <<= 1;
+                    }
+                }
+                CrcTable[i] = current;
+            }
+        }
+        public static byte CalculateCRC8(byte[] data)
+        {
+            byte crc = 0x00;
+            foreach (byte b in data)
+            {
+                crc = CrcTable[crc ^ b];
+            }
+            return crc;
+        }
+    }
+}
--- SerialPort/SerialPort/Program.cs
+++ SerialPort/SerialPort/Program.cs
@@ -1,6 +1,16 @@
 ﻿using System.Diagnostics.Metrics;
 using System.IO.Ports;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
+using ClassLibrary;
+
+byte[] byteArray = BitConverter.GetBytes(17.92);
+foreach (byte b in byteArray)
+{
+    Console.WriteLine(b);
+}
+byte crc = CRC8.CalculateCRC8(byteArray);
+Console.WriteLine($"Calculated CRC-8: 0x{crc:X2}");
 
 SerialPort SP = new();
 SP.PortName = "com3";
