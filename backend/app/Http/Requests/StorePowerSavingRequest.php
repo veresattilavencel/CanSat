f7c3cf3412330da2487abb7621d8fd631ea113c0
Veres Attila Vencel <V.Attila.Vencel@gmail.com>
2025-03-23 16:34:41 +0000

    database reorganizing: cleanup unnecessary files

--- backend/app/Http/Requests/StorePowerSavingRequest.php
+++ backend/app/Http/Requests/StorePowerSavingRequest.php
@@ -11,7 +11,7 @@ class StorePowerSavingRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\MissionLiveController;
+use App\Http\Controllers\PowerSavingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PreFlightController;
 
 Route::apiResource("preflights", PreFlightController::class);
 Route::apiResource("missionlives", MissionLiveController::class);
+Route::apiResource("powersavings", PowerSavingController::class);
