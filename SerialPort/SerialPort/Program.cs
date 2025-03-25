feef9d87515562571f6661cd299b9ddd9da9c964
veresattilavencel <v.attila.vencel@gmail.com>
2025-03-25 20:34:08 +0100

    Handle errors when there is no recieved data

--- SerialPort/SerialPort/Program.cs
+++ SerialPort/SerialPort/Program.cs
@@ -22,29 +22,38 @@ SerialPort SP = new()
 SP.Open();
 while (true)
 {
-    string?[] data = SP.ReadLine().Split(",");
     try
     {
-        if (data[0] == "1")
+        string?[] data = SP.ReadLine().Split(",");
+        try
         {
-            //PreFlight
-            api.AddNewPreFlight(new(DateTime.Parse(data[1]), double.Parse(data[2]), double.Parse(data[3]), double.Parse(data[4]), double.Parse(data[5]), double.Parse(data[6]), double.Parse(data[7]), double.Parse(data[8]), double.Parse(data[9])));
-        }
-        else if (data[0] == "2")
-        {
-            //MissionLive
-            api.AddNewMissionLive(new(DateTime.Parse(data[1]), double.Parse(data[2]), double.Parse(data[3]), double.Parse(data[4]), double.Parse(data[5]), double.Parse(data[6]), double.Parse(data[7]), double.Parse(data[8]), double.Parse(data[9]), double.Parse(data[10]), double.Parse(data[11]), double.Parse(data[12]), double.Parse(data[13]), double.Parse(data[14]), double.Parse(data[15]), double.Parse(data[16]), double.Parse(data[17]), double.Parse(data[18]), double.Parse(data[19]), double.Parse(data[20]), double.Parse(data[21]), double.Parse(data[22]), double.Parse(data[23]), double.Parse(data[24]), double.Parse(data[25]), double.Parse(data[26])));
-        }
-        else if(data[0] == "3")
-        {
-            //PowerSaving
-            api.AddNewPowerSaving(new(DateTime.Parse(data[1]), double.Parse(data[2]), double.Parse(data[3]), double.Parse(data[4]), double.Parse(data[5])));
+            if (data[0] == "1")
+            {
+                //PreFlight
+                api.AddNewPreFlight(new(DateTime.Parse(data[1]), double.Parse(data[2]), double.Parse(data[3]), double.Parse(data[4]), double.Parse(data[5]), double.Parse(data[6]), double.Parse(data[7]), double.Parse(data[8]), double.Parse(data[9])));
+            }
+            else if (data[0] == "2")
+            {
+                //MissionLive
+                api.AddNewMissionLive(new(DateTime.Parse(data[1]), double.Parse(data[2]), double.Parse(data[3]), double.Parse(data[4]), double.Parse(data[5]), double.Parse(data[6]), double.Parse(data[7]), double.Parse(data[8]), double.Parse(data[9]), double.Parse(data[10]), double.Parse(data[11]), double.Parse(data[12]), double.Parse(data[13]), double.Parse(data[14]), double.Parse(data[15]), double.Parse(data[16]), double.Parse(data[17]), double.Parse(data[18]), double.Parse(data[19]), double.Parse(data[20]), double.Parse(data[21]), double.Parse(data[22]), double.Parse(data[23]), double.Parse(data[24]), double.Parse(data[25]), double.Parse(data[26])));
+            }
+            else if (data[0] == "3")
+            {
+                //PowerSaving
+                api.AddNewPowerSaving(new(DateTime.Parse(data[1]), double.Parse(data[2]), double.Parse(data[3]), double.Parse(data[4]), double.Parse(data[5])));
 
+            }
+        }
+        catch
+        {
+            StreamWriter w = new("data.csv", true, Encoding.UTF8);
+            w.WriteLine(string.Join(';', data));
+            w.Close();
         }
     }
-    catch {
-        StreamWriter w = new("data.csv", true, Encoding.UTF8);
-        w.WriteLine(string.Join(';', data));
-        w.Close();
+    catch
+    {
+        Console.WriteLine("Nincs adat");
     }
+    
 }
--- SerialPort/SerialPort/obj/Debug/net8.0/SerialPort.AssemblyInfo.cs
+++ SerialPort/SerialPort/obj/Debug/net8.0/SerialPort.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SerialPort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf01807aa83c00da56303e7463229461439bd7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ee74616a71b2139d4bec6afcf05dcbb6e01a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("SerialPort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SerialPort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
